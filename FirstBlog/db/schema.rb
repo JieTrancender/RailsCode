c2a6be8132633b730c28314f4b44088ef39db053
Jason <jie-email@jie-trancender.org>
2017-02-27 22:24:41 +0800

    update

--- FirstBlog/db/schema.rb
+++ FirstBlog/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170227140218) do
+ActiveRecord::Schema.define(version: 20170227142030) do
 
   create_table "articles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "title"
