b77036ba98dfbdfc02787ced7595a6d3f8b91564
Jason <jie-email@jie-trancender.org>
2017-04-16 10:37:01 +0800

    filling-in-layout

--- hello_app/config/routes.rb
+++ hello_app/config/routes.rb
@@ -1,20 +1,17 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+
   get 'foo/bar'
 
   get 'foo/baz'
 
-  get 'static_pages/home'
-
-  get 'static_pages/help'
-
-  get 'static_pages/about'
-
-  get 'static_pages/contact'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+  get '/help', to: 'static_pages#help'
 
   resources :microposts
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
   # root 'users#index'
 
   # get 'application#goodbye'
