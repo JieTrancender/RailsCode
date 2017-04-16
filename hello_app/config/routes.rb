da701d47ccd80ac33c897b4bf8d8d3b69d4ed203
Jason <jie-email@jie-trancender.org>
2017-04-16 10:48:04 +0800

    finish layout and routes

--- hello_app/config/routes.rb
+++ hello_app/config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
+  # static_pages
   root 'static_pages#home'
-
-  get 'foo/bar'
-
-  get 'foo/baz'
-
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/help', to: 'static_pages#help'
+
+  # users
+  get '/signup', to: 'users#new'
+
 
   resources :microposts
   resources :users
